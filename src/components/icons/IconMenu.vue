87dd1939ce98c9746be47da6b24759fac13b3837
youssef-erremili <yousseferremili19@gmail.com>
2024-10-30 22:03:26 +0100

    icon menu

---
+++ src/components/icons/IconMenu.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 8h16M4 16h16" />
+    </svg>
+</template>

d8cd1167fe658d6cffd8a769b95c7ed77883bb9a
youssef-erremili <yousseferremili19@gmail.com>
2024-10-30 22:10:15 +0100

    create inbox component

---
+++ src/components/inBox.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="relative w-3/4 my-10 h-96 mx-auto rounded-md overflow-hidden bg-slate-50 border">
+            <div class="flex items-center justify-between w-full bg-slate-800 py-4 px-8">
+                <section class="text-lg capitalize">
+                    <h2>sender</h2>
+                </section>
+                <section class="text-lg capitalize">
+                    <h2>subject</h2>
+                </section>
+                <section class="text-lg capitalize">
+                    <h2>view</h2>
+                </section>
+            </div>
+            <div class="text-slate-900">
+                <slot></slot>
+                <WaitingMsg  v-if="isWait" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import WaitingMsg from './WaitingMsg.vue';
+
+export default {
+    components: {
+        WaitingMsg
+    },
+    props: {
+        isWait: {
+            Type: String,
+            required: true,
+        }
+    }
+}
+</script>

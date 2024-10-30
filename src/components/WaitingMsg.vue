356cfaafc13e6a8de8a14e6ea62c76829ba128e7
youssef-erremili <yousseferremili19@gmail.com>
2024-10-30 22:08:49 +0100

    create waiting message component

---
+++ src/components/WaitingMsg.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-fit mx-auto text-center mt-20">
+        <IconWait class="w-fit inline-table" />
+        <p class="text-[20px]">Your inbox is empty</p>
+        <p>Waiting for incoming emails</p>
+    </div>
+</template>
+
+<script>
+import IconWait from './icons/IconWait.vue'
+
+export default {
+    components: {
+        IconWait
+    }
+}
+</script>

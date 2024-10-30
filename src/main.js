812edc607beab9b3de5503ad774e6a5a76e3208b
youssef-erremili <yousseferremili19@gmail.com>
2024-10-30 21:58:48 +0100

    main.js

--- src/main.js
+++ src/main.js
@@ -4,9 +4,19 @@ import './assets/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ToastPlugin from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-bootstrap.css';
+
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+app.use(ToastPlugin);
 app.mount('#app')
